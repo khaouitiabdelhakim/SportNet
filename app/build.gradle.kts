93b3ce227a0d1f6b27b3d79916a4363acd312740
khaouitiabdelhakim <abdelhakimkhaouiti62@gmail.com>
2024-05-20 02:39:12 +0100

    1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ensias.sportnet"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.1"
+        versionCode = 6
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
